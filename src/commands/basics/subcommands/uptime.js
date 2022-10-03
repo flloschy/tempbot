7fb0f7935c3241d0eb084f2d5a9cd66414350bb5
flloschy <floschy.email@gmail.com>
2022-10-03 10:58:56 +0100

    Uptime command

---
+++ src/commands/basics/subcommands/uptime.js
@@ -0,0 +1,27 @@
+const tools = require("../../../functions/base/tools")
+
+
+module.exports = {
+    shortdescription: "How log am I up already?",
+    longdescription: "returns how long the bot is running in days, hours, minutes and seconds",
+    args: [],
+
+    execute: async function(interaction) {
+        // time
+        let totalSeconds = (interaction.client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+
+
+        let embed = await tools.baseEmbed(interaction, "success")
+        embed
+           .setTitle(`Uptime`)
+           .setDescription(`Im already up since **${days}** days, **${hours}** hours, **${minutes}** minutes and **${seconds}** seconds`)
+
+        interaction.reply({embeds:[embed]});
+    }
+}
